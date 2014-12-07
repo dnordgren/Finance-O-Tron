3d32c7e1a78788632472f72018575177f6a4a589
Derek Nordgren <dnordgren.nordgren@gmail.com>
2014-12-07 14:49:46 -0600

    Remove Rees's pesky `print` statements.
    Because he is bad.

--- App/R/financial_analysis.R
+++ App/R/financial_analysis.R
@@ -14,7 +14,7 @@ get_market_data <- function(){
   # Reverse the data to get the normal form
   gspc_data <- gspc_data[nrow(gspc_data):1,]
   tnx_data <- tnx_data[nrow(tnx_data):1,]
-  
+
   # Calculate the rates of return for gspc
   gspc_rates <<- diff(gspc_data$'Adjusted Close')/gspc_data$'Adjusted Close'[-length(gspc_data$'Adjusted Close')]
   # Find the Average rate for each, and then annualize it
@@ -24,7 +24,7 @@ get_market_data <- function(){
   tnx_rate <<- mean(tnx_data$'Adjusted Close'/100)
   # Calculate the equity risk premium
   erp <<- gspc_rate - tnx_rate
-  
+
   # Get data into shorter form for comparison with individual stocks
   gspc_data <- gspc_data[as.Date(gspc_data$Date) >= as.Date(ymd(Sys.Date()) - years(1)),]
   # Calculate the rates of return for gspc
@@ -48,7 +48,7 @@ calculate_standard_deviation <- function(weighted_rates)
   # Sum each row for single return rates per period
   single_rates <- rowSums(weighted_rates)
   #Calculate the standard deviation
-  sd(single_rates) 
+  sd(single_rates)
 }
 
 # Creates a matrix of all possible combinations of weights
@@ -70,9 +70,6 @@ calculate_weights_matrix <- function(stock_number, sum)
 }
 
 calculate_returns <- function(prices, dates){
-  print(prices)
-  print(head(dates, 1))
-  print(tail(dates, 1))
   # Determine frequency
   years <- as.duration(ymd(tail(dates, 1)) - ymd(head(dates, 1))) /  as.duration(years(1))
   number_per_year <- round(length(prices)/years)
@@ -123,10 +120,10 @@ calculate_expected_returns <- function(returns){
 create_combination_plot <- function(num_stocks, returns, expected_returns){
   # Builds a data frame that contains all combinations of three weights for step size = .01
   plot_weights <- calculate_weights_matrix(num_stocks, 0)
-  
+
   # For each weight combination, calculate the mean return rate and standard deviation to plot
   points <- as.data.frame(t(apply(plot_weights,1,calculate_plot_point, returns, expected_returns)))
-  
+
   # Plot the points, putting standard deviation on the x-axis and mean rate of return on the y-axis
   qplot(points, x=points[,2], y=points[,1], xlab="Standard Deviation", ylab="Mean Rate of Return", main="Portfolio Combinations Plot")
 }
