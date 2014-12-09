4265520204ec900dff11b1596726be1daff4b54a
Derek Nordgren <dnordgren.nordgren@gmail.com>
2014-12-08 20:09:03 -0600

    Add help text to Help tab.

--- App/ui.R
+++ App/ui.R
@@ -28,7 +28,44 @@ shinyUI(fluidPage(
       tabPanel("Timeseries Analysis", uiOutput("timeseries_plot.ui")),
       tabPanel("Modeling", uiOutput("model_plots.ui")),
       tabPanel("Financial Analysis", plotOutput("combination_plot")),
-      tabPanel("Help")
+      tabPanel("Help",
+        h3("Inputs"),
+        strong("Stock Symbol"),
+        p("Any valid NYSE ticker symbol."),
+        tags$div(style="margin-bottom:15px;"),
+        strong("Weight"),
+        p("All weights per stock you have entered will be used to calculate the
+          respective percentage weight for each stock."),
+        p("i.e. 1, 1, and 2 will result in 25%, 25%, and 50%, respectively."),
+        tags$div(style="margin-bottom:15px;"),
+        strong("Date Range"),
+        p("The date range you enter for your first stock will be locked in for
+          all successive stocks added. Hit 'Clear Stocks' to start over."),
+        tags$div(style="margin-bottom:15px;"),
+        h3("Areas of Analysis"),
+        strong("Portfolio"),
+        p("Each stock and its respective weight that you entered will be displayed
+          here as you build your portfolio. Each stock's expected return and standard
+          deviation will also be displayed. In addition, this analysis area allows
+          the user to specify a desired rate of return for their portfolio. Finance-O-Tron
+          will determine what weight of each stock you would be required to invest
+          in to achieve this return rate. The standard deviation of your portfolio
+          will also be calculated."),
+        tags$div(style="margin-bottom:15px;"),
+        strong("Timeseries Analysis"),
+        p("Each ticker symbol that you add will have its daily adjusted closing
+          price history over the specified period plotted here. In addition, it
+          will have the 50- and 100- day moving average plotted."),
+        tags$div(style="margin-bottom:15px;"),
+        strong("Modeling"),
+        p("Using the Holt-Winters timeseries forecasting algorithms, Finance-O-Tron
+          will forecast future adjusted closing prices for the stocks that you've
+          entered."),
+        tags$div(style="margin-bottom:15px;"),
+        strong("Financial Analysis"),
+        p("All possible combinations of weights of the stocks that you've entered
+          are plotted for all possible returns.")
+        )
     )
   )
 ))
